708dc00acd228c0c6b3850c03615e247667e579f
Juan Camilo Hernández Ramírez <Juancazzhr@gmail.com>
2023-09-16 06:11:54 -0500

    feat: Add logic dentist module
    - Add handlers, service and repository for crud operations.

---
+++ cmd/server/handler/reponses.go
@@ -0,0 +1,18 @@
+package handler
+
+type ErrorResponse struct {
+	Timestamp string   `json:"timestamp"`
+	Status    int      `json:"status"`
+	Message   string   `json:"message"`
+	Path      string   `json:"path"`
+	Errors    []string `json:"errors,omitempty"`
+}
+
+type PageResponse struct {
+	Page     int         `json:"page"`
+	Size     int         `json:"size"`
+	Total    int         `json:"total"`
+	Content  interface{} `json:"content"`
+	NextPage string      `json:"next_page"`
+	PrevPage string      `json:"prev_page"`
+}
--- internal/dentist/service.go
+++ internal/dentist/service.go
@@ -1,6 +1,10 @@
 package dentist
 
-import "github.com/10Daniel10/web-server-go-ExamenFinal/internal"
+import (
+	"errors"
+	"github.com/10Daniel10/web-server-go-ExamenFinal/internal"
+	"strings"
+)
 
 type Repository interface {
 	Create(dentist Dentist) (Dentist, error)
@@ -31,9 +35,7 @@ func (s *Service) GetAll() ([]Dentist, error) {
 func (s *Service) GetByID(id uint) (Dentist, error) {
 	data, err := s.repository.GetByID(id)
 	if err != nil {
-		if err.Error() == "record not found" {
-			return Dentist{}, internal.ErrNotFound
-		}
+		return Dentist{}, err
 	}
 
 	return data, nil
@@ -42,9 +44,7 @@ func (s *Service) GetByID(id uint) (Dentist, error) {
 func (s *Service) GetByLicense(license string) (Dentist, error) {
 	data, err := s.repository.GetByLicense(license)
 	if err != nil {
-		if err.Error() == "record not found" {
-			return Dentist{}, internal.ErrNotFound
-		}
+		return Dentist{}, err
 	}
 
 	return data, nil
@@ -52,29 +52,165 @@ func (s *Service) GetByLicense(license string) (Dentist, error) {
 
 func (s *Service) Create(dentist Dentist) (Dentist, error) {
 
-	exists, err := s.repository.GetByLicense(dentist.License)
+	dentistExist, err := s.repository.GetByLicense(dentist.License)
 	if err != nil {
-		if err.Error() == "record not found" {
-			return Dentist{}, internal.ErrNotFound
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			break
 		}
 	}
 
-	if exists.License == dentist.License {
-		return Dentist{}, internal.ErrDuplicate
+	if dentistExist.License != "" {
+		return Dentist{}, internal.ErLicenseAlreadyExists
 	}
 
-	data, err := s.repository.Create(dentist)
+	Normalize(&dentist)
+
+	dentistCreated, err := s.repository.Create(dentist)
 	if err != nil {
-		return Dentist{}, err
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			break
+		}
 	}
 
-	return data, nil
+	return dentistCreated, nil
 }
 
 func (s *Service) Update(dentist Dentist) (Dentist, error) {
-	return s.repository.Update(dentist)
+
+	dentistSearched, err := s.repository.GetByID(dentist.ID)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return Dentist{}, internal.ErNotFound
+
+		default:
+			return Dentist{}, internal.ErServiceUnavailable
+		}
+	}
+
+	Normalize(&dentist)
+
+	if dentistSearched.License != dentist.License {
+		dentistExist, err := s.repository.GetByLicense(dentist.License)
+		if err != nil {
+			switch {
+			case errors.Is(err, internal.ErNotFound):
+				break
+			}
+		}
+
+		if dentistExist.License != "" {
+			return Dentist{}, internal.ErLicenseAlreadyExists
+		}
+	}
+
+	dentistUpdated, err := s.repository.Update(dentist)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return Dentist{}, internal.ErNotFound
+
+		default:
+			return Dentist{}, internal.ErServiceUnavailable
+		}
+	}
+
+	return dentistUpdated, nil
+}
+
+func (s *Service) Patch(dentist Dentist) (Dentist, error) {
+
+	dentistSearched, err := s.repository.GetByID(dentist.ID)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return Dentist{}, internal.ErNotFound
+
+		default:
+			return Dentist{}, internal.ErServiceUnavailable
+		}
+	}
+
+	CompareTo(&dentist, dentistSearched)
+
+	if dentistSearched.License != dentist.License {
+		dentistExist, err := s.repository.GetByLicense(dentist.License)
+		if err != nil {
+			switch {
+			case errors.Is(err, internal.ErNotFound):
+				break
+			}
+		}
+
+		if dentistExist.License != "" {
+			return Dentist{}, internal.ErLicenseAlreadyExists
+		}
+	}
+
+	dentistUpdated, err := s.repository.Update(dentist)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return Dentist{}, internal.ErNotFound
+
+		default:
+			return Dentist{}, internal.ErServiceUnavailable
+		}
+	}
+
+	return dentistUpdated, nil
 }
 
 func (s *Service) Delete(id uint) error {
-	return s.repository.Delete(id)
+	_, err := s.repository.GetByID(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return internal.ErNotFound
+
+		default:
+			return internal.ErServiceUnavailable
+		}
+	}
+
+	err = s.repository.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, internal.ErNotFound):
+			return internal.ErNotFound
+
+		default:
+			return internal.ErServiceUnavailable
+		}
+	}
+	return nil
+}
+
+// Custom functions for the service
+// Normalize and CompareTo are used to avoid empty fields in the database
+
+func Normalize(dentist *Dentist) {
+	dentist.Name = strings.ToLower(dentist.Name)
+	dentist.Lastname = strings.ToLower(dentist.Lastname)
+	dentist.License = strings.ToLower(dentist.License)
+
+	dentist.Name = strings.TrimSpace(dentist.Name)
+	dentist.Lastname = strings.TrimSpace(dentist.Lastname)
+	dentist.License = strings.TrimSpace(dentist.License)
+}
+
+func CompareTo(a *Dentist, b Dentist) {
+	if a.Name == "" {
+		a.Name = b.Name
+	}
+
+	if a.Lastname == "" {
+		a.Lastname = b.Lastname
+	}
+
+	if a.License == "" {
+		a.License = b.License
+	}
 }
